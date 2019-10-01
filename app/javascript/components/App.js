ea34fbfe3dfd37ea3bd455821bb394b85e6954da
Tonyjlum <tonyjlum@gmail.com>
2019-09-30 21:20:21 -0400

    moved css

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+import '../app.css';
 
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 
 import HelloWorld from './HelloWorld.js'
+import Login from './login.js'
 
 class App extends React.Component {
   render () {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path = "/" render={() => {"Home!"}}/>
+          <Route exact path = "/" render={() => <Login/>}/>
+          
           <Route path="/hello" render={() => <HelloWorld greeting="Friend"/>} />
         </Switch>
       </BrowserRouter>
