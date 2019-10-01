306a94e9c4cc2cee12c53f6f1ac0d4f46daad8c4
Tonyjlum <tonyjlum@gmail.com>
2019-09-30 21:21:21 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Tony", password:"greentea", email: "tonyjlum@gmail.com")
+
+Transaction.create(ticker_symbol: "TEST", stock_price: 33.91, shares: 11, user_id: 1)
+Transaction.create(ticker_symbol: "REST", stock_price: 9.91, shares: 40, user_id: 1)
