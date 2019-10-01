966bd601658b795a266aa59934d80e20eed7f240
Tonyjlum <tonyjlum@gmail.com>
2019-09-30 21:11:54 -0400

    frontend skel set up without redux

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   Rails.application.routes.draw do
-    root 'static#index'
-
     namespace :v1, defaults: { format: 'json'} do
       get 'users', to: 'users#index'
     end
+
+    get "*page", to: "static#index", constraints: ->(req) do
+      !req.xhr? && req.format.html?
+    end
+
+    root 'static#index'
   end
 end
