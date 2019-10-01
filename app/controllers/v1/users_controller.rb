e99e3da73b9d7934cef6b7a2e4461673eaa649d3
Tonyjlum <tonyjlum@gmail.com>
2019-09-30 21:34:48 -0400

    database connected to index and show, seeded data

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,10 +1,11 @@
 class V1::UsersController < ApplicationController
   def index
-    render json: { :things => [
-      {
-        :name => 'something',
-        :guid => "gfjnjgnfdnj"
-      }
-      ] }.to_json
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  Rails.application.routes.draw do
-    namespace :v1, defaults: { format: 'json'} do
-      get 'users', to: 'users#index'
-    end
-
-    get "*page", to: "static#index", constraints: ->(req) do
-      !req.xhr? && req.format.html?
-    end
-
-    root 'static#index'
+  namespace :v1, defaults: { format: 'json'} do
+    # get 'users', to: 'users#index'
+    resources :transactions
+    resources :users
   end
+
+  get "*page", to: "static#index", constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 end
